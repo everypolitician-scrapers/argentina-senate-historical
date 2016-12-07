e0ed0e2de9ab136acc6a97ceccdfa85bfa238d71
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-07 17:29:06 +0000

    Send POST request to get members between 1/1/2015 and 1/1/2016
    Sending a POST request to http://www.senado.gov.ar/senadores/Historico/FechaResultado
    will return a list of members within the dates specified in the request parameters.
    
    The request asks for a list of members between 01/01/2015 and 01/01/2016

---
+++ scraper.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+require 'pry'
+require 'require_all'
+
+require_rel 'lib'
+
+url = URI.parse('http://www.senado.gov.ar/senadores/Historico/FechaResultado')
+
+params = {
+  'senado_senadoresbundle_busquedahistoricostype%5BfechaDesde%5D%5Bday%5D' => 1,
+  'senado_senadoresbundle_busquedahistoricostype%5BfechaDesde%5D%5Bmonth%5D' => 1,
+  'senado_senadoresbundle_busquedahistoricostype%5BfechaDesde%5D%5Byear%5D' => 2015,
+  'senado_senadoresbundle_busquedahistoricostype%5BfechaHasta%5D%5Bday%5D' => 1,
+  'senado_senadoresbundle_busquedahistoricostype%5BfechaHasta%5D%5Bmonth%5D' => 1,
+  'senado_senadoresbundle_busquedahistoricostype%5BfechaHasta%5D%5Byear%5D' => 2016
+}
+
+res = Net::HTTP.post_form(url, params)
