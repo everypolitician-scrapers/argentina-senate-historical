aac87e903a3242c7d7fbf5ef1a73d7536c276c4b
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-12 11:20:40 +0000

    Add private method to return tds in member row
    We will need this row for other member fields

--- lib/member_section.rb
+++ lib/member_section.rb
@@ -2,6 +2,12 @@ require 'scraped'
 
 class MemberSection < Scraped::HTML
   field :name do
-    noko.xpath('td')[0].text
+    tds[0].text
+  end
+
+  private
+
+  def tds
+    noko.xpath('td')
   end
 end
